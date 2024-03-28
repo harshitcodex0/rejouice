59a462180b5964c3d20363cdeaf9df42d2f59763
harshitcodex0 <harshit456987@hotmail.com>
2024-03-28 22:51:11 +0530

    page1 created

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
-var page1content = document.querySelector("#page1-content")
-var cursor = document.querySelector("#cursor")
+function cursorEffect(){
+    var page1content = document.querySelector("#page1-content");
+    var cursor = document.querySelector("#cursor");
 
-page1content.addEventListener("mousemove", function(dets) {
-    cursor.style.left = dets.x+"px";
-    cursor.style.top = dets.y+"px";
+    page1content.addEventListener("mousemove", function (dets) {
+      gsap.to(cursor, {
+        x: dets.x,
+        y: dets.y,
+      });
+    });
 
+    page1content.addEventListener("mouseenter", function () {
+      gsap.to(cursor, {
+        scale: 1,
+        opacity: 1,
+      });
+    });
+    page1content.addEventListener("mouseleave", function () {
+      gsap.to(cursor, {
+        scale: 0,
+        opacity: 1,
+      });
+    });
+}
+cursorEffect();
 
-})
+function page2Animation (){
+    gsap.from(".elem h1", {
+      y: 120,
+      stagger: 0.2,
+      duration: 1,
+      scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        start: "top 47%",
+        end: "top 46%",
+        markers: true,
+        scrub: 2
+      },
+    });
+    
+}
